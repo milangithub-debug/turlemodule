5b9fa2bde0498aab9f42bd4d28a6e0d518babd9b
milan <smilandeep31@gmail.com>
2021-01-12 20:15:04 -0500

    this is turle module my practice code

---
+++ jump.py
@@ -0,0 +1,12 @@
+import turtle
+
+milan=turtle.Turtle()
+milan.circle(200)
+
+
+
+
+
+
+
+turtle.done()
---
+++ square.py
@@ -0,0 +1,27 @@
+import turtle
+
+milan=turtle.Turtle()
+milan.color("red","blue")
+milan.begin_fill()
+milan.forward(200)
+milan.left(90)
+milan.color("blue")
+milan.forward(200)
+milan.left(90)
+milan.color("red")
+milan.forward(200)
+milan.left(90)
+milan.color("blue")
+milan.forward(200)
+milan.left(90)
+milan.end_fill()
+
+
+
+
+
+
+
+
+
+turtle.done()
